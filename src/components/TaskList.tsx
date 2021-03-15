2fb9fadb3272c308ff170a30dee02fe084449d76
Rodrigo Rodrigues <rodrigorodrigues1807@gmail.com>
2021-03-15 14:47:47 -0300

    Challenge Complete

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import { v4 as uuidV4 } from 'uuid';
 
 import '../styles/tasklist.scss'
 
 import { FiTrash, FiCheckSquare } from 'react-icons/fi'
 
 interface Task {
-  id: number;
+  id: string;
   title: string;
   isComplete: boolean;
 }
@@ -16,14 +17,36 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return;
+
+    
+    const newTask = {
+      id: uuidV4(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    console.log(newTask);
+
+    setTasks(oldState => [...oldState, newTask]);
+    setNewTaskTitle('');
   }
 
-  function handleToggleTaskCompletion(id: number) {
+  function handleToggleTaskCompletion(id: string) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const checkedTask = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete } : task
+    );
+
+    setTasks(checkedTask);
   }
 
-  function handleRemoveTask(id: number) {
+  function handleRemoveTask(id: string) {
     // Remova uma task da listagem pelo ID
+    const removeTask = tasks.filter(task => task.id !== id);
+
+    setTasks(removeTask);
   }
 
   return (
